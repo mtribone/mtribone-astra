17dc30899fce009849233808478e6dd7e8341a16
mtribone <t-bone@mtribone.com>
2020-07-01 09:53:29 -0400

    Overrides the function to retrieve the Read More link and removes its outout for accessibility. This is a temporary hack until a better solution is found–perhaps in the theme config.

--- functions.php
+++ functions.php
@@ -23,3 +23,40 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+/**
+ * Function to get Read More Link of Post
+ *
+ * @since 1.0.0
+ * @return html
+ */
+if ( ! function_exists( 'astra_post_link' ) ) {
+
+	/**
+	 * Function to get Read More Link of Post
+	 *
+	 * @param  string $output_filter Filter string.
+	 * @return html                Markup.
+	 */
+	function astra_post_link( $output_filter = '' ) {
+
+		$enabled = apply_filters( 'astra_post_link_enabled', '__return_true' );
+		if ( ( is_admin() && ! wp_doing_ajax() ) || ! $enabled ) {
+			return $output_filter;
+		}
+
+		$read_more_text    = apply_filters( 'astra_post_read_more', __( 'Read More &raquo;', 'astra' ) );
+		$read_more_classes = apply_filters( 'astra_post_read_more_class', array() );
+
+		$post_link = sprintf(
+			esc_html( '%s' ),
+			'<a class="' . esc_attr( implode( ' ', $read_more_classes ) ) . '" href="' . esc_url( get_permalink() ) . '"> ' . the_title( '<span class="screen-reader-text">', '</span>', false ) . ' ' . $read_more_text . '</a>'
+		);
+
+    // This is a temporary hack to remove the Read More link for accessibility
+		// $output = ' &hellip;<p class="read-more"> ' . $post_link . '</p>';
+
+		return apply_filters( 'astra_post_link', $output, $output_filter );
+	}
+}
+add_filter( 'excerpt_more', 'astra_post_link', 1 );
