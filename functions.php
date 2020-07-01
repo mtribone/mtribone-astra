a7ccc179b8e88bfaec8f63d9635a5316d61bf9c2
mtribone <t-bone@mtribone.com>
2020-07-01 14:12:59 -0400

    Adds a filter for astra_post_link_enabled and sets it to false so that it does not display the Read More >> link. Creates a new function to remove the crappy ellipses from the previous filter and then adds the new filter to excerpt_more

--- functions.php
+++ functions.php
@@ -23,40 +23,10 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
-
-/**
- * Function to get Read More Link of Post
- *
- * @since 1.0.0
- * @return html
- */
-if ( ! function_exists( 'astra_post_link' ) ) {
-
-	/**
-	 * Function to get Read More Link of Post
-	 *
-	 * @param  string $output_filter Filter string.
-	 * @return html                Markup.
-	 */
-	function astra_post_link( $output_filter = '' ) {
-
-		$enabled = apply_filters( 'astra_post_link_enabled', '__return_true' );
-		if ( ( is_admin() && ! wp_doing_ajax() ) || ! $enabled ) {
-			return $output_filter;
-		}
-
-		$read_more_text    = apply_filters( 'astra_post_read_more', __( 'Read More &raquo;', 'astra' ) );
-		$read_more_classes = apply_filters( 'astra_post_read_more_class', array() );
-
-		$post_link = sprintf(
-			esc_html( '%s' ),
-			'<a class="' . esc_attr( implode( ' ', $read_more_classes ) ) . '" href="' . esc_url( get_permalink() ) . '"> ' . the_title( '<span class="screen-reader-text">', '</span>', false ) . ' ' . $read_more_text . '</a>'
-		);
-
-    // This is a temporary hack to remove the Read More link for accessibility
-		// $output = ' &hellip;<p class="read-more"> ' . $post_link . '</p>';
-
-		return apply_filters( 'astra_post_link', $output, $output_filter );
-	}
+// Removes the 'Read More >>' link
+add_filter( 'astra_post_link_enabled', '__return_false' );
+// Overrides the crappy ellipses
+function new_excerpt_more($more) {
+    return ' ...';
 }
-add_filter( 'excerpt_more', 'astra_post_link', 1 );
+add_filter('excerpt_more', 'new_excerpt_more');
